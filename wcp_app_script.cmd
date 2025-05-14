6ff431850e0796f9d87b28f17c89a23b92969a61
Shannon Whitley <swhitley@whitleymedia.com>
2025-05-13 20:48:04 -0700

    Update wcp_app_script.cmd
    - Add ORGID to script

--- wcp_app_script.cmd
+++ wcp_app_script.cmd
@@ -1,5 +1,10 @@
+SET ORGID=xzkyht
 SET APP_REFERENCE_ID=positionmanagement_xzkyht
 SET APP_DIRECTORY=%cd%
 SET WCP_APP_SCRIPT_DIR=C:\Program Files\Workday\wcpcli\bin
 
+cmd /c wcpcli config:set defaultCompanyShortId %ORGID%
+
 cmd /k py "%WCP_APP_SCRIPT_DIR%\wcp_app_script.py" "%APP_REFERENCE_ID%" "%APP_DIRECTORY%" 
+
+
